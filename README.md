23574da78b705e55af19b7db9a215be1d160f604
Jose <josegonz115@gmail.com>
2023-08-07 21:19:20 -0700

    challenges complete

--- README.md
+++ README.md
@@ -1,44 +1,44 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *Crowdfunding of Monster Games*
+Submitted by: **Jose Juan Gonzalez Jr**
 
-Submitted by: **Your Name Here**
+### To reiterate, this is a mock prework assignment for a course, not real crowdfunding. 
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**Crowdfunding of Monster Games** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **5** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [✅] The introduction section explains the background of the company and how many games remain unfunded.
+* [✅] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [✅] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [✅] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
 
-* [ ] List anything else that you can get done to improve the app functionality!
+* [✅] Search input feature that dynamically searches for games on each input!
+* [✅] Navigational bar that allows for user to instantly navigate to specific page sections.
+* [✅] Media queries that allow for mobile phone accessibility. 
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://i.imgur.com/XZXJe2x.gif' title='Video Walkthrough' width='70' alt='Video Walkthrough' />
 
-<!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+GIF created with [Kap](https://getkap.co/) for macOS
+
 
 ## Notes
 
-Describe any challenges encountered while building the app.
+Had challenges conceptualize the traversal "forEach" functions such as reduce and filter. 
+Also, spend time planning the search button. Realized I could recycle previous js functions. 
 
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright [2023] [Jose Juan Gonzalez Jr]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -29,27 +29,34 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for(let i = 0; i < games.length; i += 1){
         // create a new div element, which will become the game card
-
-
+        let gameCard = document.createElement("div");
         // add the class game-card to the list
-
+        gameCard.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        gameCard.innerHTML = 
+        `<img src="${games[i].img}" class="game-img" alt="${games[i].name} image" />
+        <h3 class="game-header">${games[i].name}</h3>
+        <p>${games[i].description}</p>
+        <br />
+        <div class="game-stats">
+            <p>Backers: ${games[i].backers.toLocaleString('en-US')}</p>
+            <p>Pledged: $${games[i].pledged.toLocaleString('en-US')}</p>
+            <p>Goal: $${games[i].goal.toLocaleString('en-US')}</p>
+        </div>`;
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
@@ -61,19 +68,22 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (total, game) => { return total + game.backers}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<p>${totalContributions.toLocaleString('en-US')}</p>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce( (total, game) => { return total + game.pledged }, 0 );
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `<p>$${totalRaised.toLocaleString('en-US')}</p>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+const totalGames = GAMES_JSON.length;
+gamesCard.innerHTML = `<p>${totalGames.toLocaleString('en-US')}</p>`;
 
 
 /*************************************************************************************
@@ -85,31 +95,26 @@ const gamesCard = document.getElementById("num-games");
 // show only games that do not yet have enough funding
 function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
-
     // use filter() to get a list of games that have not yet met their goal
-
-
+    const unfundedGames = GAMES_JSON.filter( (game) => { return game.pledged < game.goal; });
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
 function filterFundedOnly() {
     deleteChildElements(gamesContainer);
-
     // use filter() to get a list of games that have met or exceeded their goal
-
-
+    const fundedGames = GAMES_JSON.filter( (game) => { return game.pledged >= game.goal; });
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedGames);
 }
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
-
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,7 +123,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener('click', filterUnfundedOnly);
+fundedBtn.addEventListener('click', filterFundedOnly);
+allBtn.addEventListener('click', showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,12 +136,21 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGamesCount = GAMES_JSON.reduce( (total, game) => {
+    return game.pledged < game.goal ? total + 1 : total; }, 0);
 
 // create a string that explains the number of unfunded games using the ternary operator
+const allGamesStr = totalGames===1 ? "game": "games";
+const unfundedAmtStr = unfundedGamesCount===1 ? "game": "games";
+const unfundedGamesString = 
+`A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${totalGames} ${allGamesStr}. 
+However, ${unfundedGamesCount} ${unfundedAmtStr} still have not yet met their goal.`;
 
 
 // create a new DOM element containing the template string and append it to the description container
+const unfundedGamesElement = document.createElement("p");
+unfundedGamesElement.innerHTML = unfundedGamesString;
+descriptionContainer.appendChild(unfundedGamesElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +165,29 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, runnerUp, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
-
+const topGameName = document.createElement("p");
+topGameName.innerHTML = topGame.name;
+firstGameContainer.appendChild(topGameName);
 // do the same for the runner up item
+const runnerUpName = document.createElement("p");
+runnerUpName.innerHTML = runnerUp.name;
+secondGameContainer.appendChild(runnerUpName);
+
+/************************************************************************************
+ * Adding a search feature for the games
+ * Skills used: event listeners, filter, includes, toLowerCase
+ */
+
+function searchResults() {
+    const searchTerm = searchInput.value.toLowerCase();
+    // Filter the games based on the input value
+    let filteredGames = GAMES_JSON.filter( (game) => {return game.name.toLowerCase().includes(searchTerm) });
+    deleteChildElements(gamesContainer);
+    addGamesToPage(filteredGames);
+    console.log("searching for", searchTerm);
+  };
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', searchResults); 
